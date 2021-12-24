f0da183e84aeaa8206e960d7a4e7d76e52360146
Omair <abd.alkhahhar@gmail.com>
2021-12-24 10:36:01 +0200

    step 2

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const express = require('express')
+const app =express()
+
+app.get('/', (req,res)=>{
+    res.send('ok')
+})
+const PORT = process.env.PORT || 5000
+app.listen(PORT,()=>{
+    console.log(`Application is listed and working on port ${PORT}`)
+})
