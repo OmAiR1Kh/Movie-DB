7615ec378edffe40b26212d852409daef55b346a
Omair <abd.alkhahhar@gmail.com>
2021-12-24 12:05:49 +0200

    step 3

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
+/*Imports*/
+const { response } = require('express')
 const express = require('express')
 const app =express()
+const date = new Date().toLocaleTimeString().replace(/([\d]+:[\d]{2})(:[\d]{2})(.*)/, "$1$3")
+/*
+DB connection
+*/
+
+/*
+Routes
+*/
 
 app.get('/', (req,res)=>{
     res.send('ok')
 })
+app.use('/test', (req,res,next)=>{
+    res.status(200).json({
+        status:200,
+        message:"ok"
+    })
+})
+app.use('/time', (req,res,next)=>{
+    res.status(200).json({
+        status:200,
+        message:date
+    })
+})
+
+/*
+Listening on PORT
+*/
 const PORT = process.env.PORT || 5000
 app.listen(PORT,()=>{
     console.log(`Application is listed and working on port ${PORT}`)
