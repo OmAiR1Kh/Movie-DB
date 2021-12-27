e8f7e2df8b13b40e0206dd184a73a9a8fb4461f0
Omair <abd.alkhahhar@gmail.com>
2021-12-27 21:01:06 +0200

    step 4

--- index.js
+++ index.js
@@ -14,18 +14,48 @@ Routes
 app.get('/', (req,res)=>{
     res.send('ok')
 })
-app.use('/test', (req,res,next)=>{
+app.use('/test', (req,res)=>{
     res.status(200).json({
         status:200,
         message:"ok"
     })
 })
-app.use('/time', (req,res,next)=>{
+app.use('/time', (req,res)=>{
     res.status(200).json({
         status:200,
         message:date
     })
 })
+
+app.use('/Hello/:id', (req,res)=>{
+    let :id = undefined;
+    res.status(200).json({
+        status:200,
+        message: `Hello ${req.params.id}`,
+    })
+})
+
+app.get('/search',(req,res) => {
+    const search = req.query.s;
+
+    if (typeof search != 'undefined') {
+        // Search string applied
+        const response = {
+            status:200, message:"ok", data: search
+        };
+
+        res.send(response);
+    }
+    else {
+        const response = {
+            status:500, error:true, message: "you have to provide a search"
+        };
+
+
+        res.status(500);
+        res.send(response);
+    }
+});
 
 /*
 Listening on PORT
